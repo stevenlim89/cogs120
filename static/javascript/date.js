d2cb5213ff56b9c865640508a937fd0aac44c96d
vagrant <vagrant@precise32.(none)>
2015-02-05 07:29:00 +0000

    homepage done

---
+++ static/javascript/date.js
@@ -0,0 +1,11 @@
+'use strict';
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("Javascript connected!");
+}
